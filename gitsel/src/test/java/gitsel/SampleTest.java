15d2cbaae2936c6cc77d864e496b21be362dd172
Dell <Dell@dell>
2021-03-15 16:06:47 +0530

    sample class created

---
+++ gitsel/src/test/java/gitsel/SampleTest.java
@@ -0,0 +1,22 @@
+package gitsel;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void orgTest(){
+		System.out.println("Organisation test");
+	}
+	@Test
+	public void createorgTest(){
+		System.out.println("Carete org");
+	}
+	@Test
+	public void oppTest(){
+		System.out.println("opp test");
+	}
+	@Test
+	public void opporTest(){
+		System.out.println("opp test");
+	}
+}
